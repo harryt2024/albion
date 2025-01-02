b42dbe672196b6205638e3a4a96f53e41c801a32
Harry T <125668633+harryt2024@users.noreply.github.com>
2025-01-02 03:28:59 +0000

    Update albionaccess.lua

--- Access/albionaccess.lua
+++ Access/albionaccess.lua
@@ -1,5 +1,5 @@
-local header = [["
+local header = [[
 Albion Access Loader V0.1
 Albion Access is currently giving the necessary files and instructions to your game to enable our products to function correctly.
-"]]
+]]
 print(header)
